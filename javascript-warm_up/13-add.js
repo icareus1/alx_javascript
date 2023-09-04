4cfe95c785f14d0b76ef336b2458f2700d1256d9
icareus1 <wamars@live.fr>
2023-09-04 16:04:25 +0000

    JavaScript Intro

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,12 +1,8 @@
 // 13-add.js
-// function add(a, b){
-//     let sum = a + b
-//     return sum;
-// }
-// module.exports = {
-//     add: add
-// };
-
-export const add = (a, b) => {
-    return a + b;
+function add(a, b){
+    let sum = a + b
+    return sum;
+}
+module.exports = {
+    add: add
 };
