1b1fa99d31dbd5177ccec16ad5e8c35e05856724
icareus1 <wamars@live.fr>
2023-09-04 16:32:33 +0000

    JavaScript Intro

---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+//101-call_me_moby.js
+function (x, theFunction){
+    x.forEach(function (theFunction){
+        console.log(theFunction)
+    })
+}
+
+module.exports = {
+    theFunction: theFunction
+};
