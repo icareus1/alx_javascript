2077965a04d9e20989068b7851aa72eed3eaa1ef
icareus1 <wamars@live.fr>
2023-09-05 16:19:51 +0000

    JavaScript OOP

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+/*
+Create a class that initialize an instance of the same
+class using a constructor (just like __init__ in python)
+*/
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
