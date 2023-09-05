210cd6754bab411445e3ebf574524987f365f1ba
icareus1 <wamars@live.fr>
2023-09-05 20:34:01 +0000

    JavaScript OOP

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -6,18 +6,14 @@ that prints the rectangle using the character c
 
 const P_Square = require('./5-square.js');
 class Square extends P_Square {
-    
-    constructor(size) {
-        super(size, size);
-    }
 
     charPrint(c) {
         if (c === undefined) {
             c = 'X';
         }
 
-        for (let i = 0; i < this.size; i++) {
-            console.log(c.repeat(this.size))
+        for (let i = 0; i < this.height; i++) {
+            console.log(c.repeat(this.width))
         } //Possible to do the same thing using nested loops.
     }
 }
