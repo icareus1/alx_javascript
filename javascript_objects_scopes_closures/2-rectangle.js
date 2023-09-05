1d537f449e269dc77a2fe2365589cf72ba52f10a
icareus1 <wamars@live.fr>
2023-09-05 16:39:15 +0000

    JavaScript OOP

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -7,7 +7,7 @@ and return an empty object if either values are <= 0
 
 class Rectangle {
     constructor(w, h) {
-        if (w > 0 || h > 0) {
+        if (w > 0 && h > 0) {
             this.width = w;
             this.height = h;            
         }
