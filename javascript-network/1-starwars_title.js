05876a3e7f681f3809ab4906e943ff58c77d79ba
icareus1 <wamars@live.fr>
2023-09-12 23:45:19 +0000

    JavaScript Network

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,26 @@
+#!usr/bin/node
+const req = require("request");
+
+if (process.argv.length !== 3) {
+  console.error("Usage: node 1-starwars_title.js <url>");
+  process.exit(1);
+}
+
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+req.get(url, (error, res, body) => {
+    if (error) {
+      console.error(error.message);
+      process.exit(1);
+    }
+
+    if (res.statusCode !== 200) {
+      console.error(`Status code: ${res.statusCode}`);
+      process.exit(1);
+    }
+
+    const movieData = JSON.parse(body);
+
+    console.log(`Title: ${movieData.title}`);
+});
