3ddf878089cf2d14a26c85983752d2282ac41073
icareus1 <wamars@live.fr>
2023-09-13 22:43:19 +0000

    JavaScript Network

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,32 @@
+#!usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+    console.error('Usage: node fetchAndStore.js <URL> <outputFilePath>');
+    process.exit(1);
+}
+
+const url = process.argv[2];
+const outputPath = process.argv[3];
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+        console.error(response.statusCode);
+        process.exit(1);
+    }
+
+    // Write the body response to the specified file
+    fs.writeFile(outputPath, body, 'utf-8', (err) => {
+        if (err) {
+            console.error(err.message);
+            process.exit(1);
+        }
+        console.log(`Webpage content successfully saved to ${outputPath}`);
+    });
+});
