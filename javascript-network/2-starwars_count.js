24ab2613fbc80a2de2266dd273feed232163b6a0
icareus1 <wamars@live.fr>
2023-09-13 18:37:50 +0000

    JavaScript Network

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -20,15 +20,11 @@ req.get(url, (error, response, body) => {
     }
 
     const films = JSON.parse(body).results;
-    const film_count = new Set();
-
-    films.forEach(film => {
-        film.characters.forEach(character => {
-            if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
-                film_count.add(film.url);
-            }
-        });
-    });
-
-    console.log(film_count.size);
+    let count = 0;
+    for (const film of films) {
+        if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+            count+=1;
+        }
+    }
+    console.log(count);
 });
