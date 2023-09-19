553daf3a1d7d28867ed19c6a64b1920b86422d2f
icareus1 <wamars@live.fr>
2023-09-19 13:12:09 +0000

    JavaScript Advanced

--- Javascript_advanced/1-nested_functions.js
+++ Javascript_advanced/1-nested_functions.js
@@ -1,34 +1,26 @@
 // Create a variable named globalVariable with value Welcome
 const globalVariable = 'Welcome';
-
 // Create a function outer that:
 function outer() {
   // alerts the content of the variable globalVariable
   alert(globalVariable);
-
   // creates a variable named course with value Holberton
   const course = 'Holberton';
-
   // creates a function inner that:
   function inner() {
     // alerts the content of the variable globalVariable and course (concatenated)
     alert(`${globalVariable} ${course}`);
-
     // creates a variable named exclamation with value !
     const exclamation = '!';
-
     // creates a function inception that alerts the content of the variable globalVariable, course, and exclamation (concatenated)
     function inception() {
       alert(`${globalVariable} ${course} ${exclamation}`);
     }
-
     // calls the function inception
     inception();
   }
-
   // calls the function inner
   inner();
 }
-
 // Call the function outer in the main code (outside any function)
 outer();
